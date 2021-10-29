ed0d6c0ac343bff112e93ed12f8ac0bdc14c6171
080812 <74765678+080812@users.noreply.github.com>
2021-10-29 15:29:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ LWY=0
 
 RWX=0
 RWY=0
+
+scoreLW=0
+scoreRW=0
+
 function preload(){
 song=loadSound("music.mp3")
 song2=loadSound("music2.mp3")
@@ -19,6 +23,16 @@ poseNet.on('pose',gotposes)
 
 function draw(){
 image(video,0,0,500,400)
+
+if(scoreLW>0.2){
+    fill('red')
+    circle(LWX,LWY,50)
+}
+
+if(scoreRW>0.2){
+    fill('red')
+    circle(RWX,RWY,50)
+}
 }
 
 function modelLoaded(){
@@ -35,5 +49,8 @@ LWY=results[0].pose.leftWrist.y
 
 RWX=results[0].pose.rightWrist.x 
 RWY=results[0].pose.rightWrist.y 
+
+scoreLW=results[0].pose.keypoints[9].score
+score=results[0].pose.keypoints[10].score
     }
 }
